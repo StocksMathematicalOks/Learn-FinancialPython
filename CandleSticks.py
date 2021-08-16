d54250d192c5063d3353b3b6b2662fa1165c5a58
StocksMathematicalOks <88820859+StocksMathematicalOks@users.noreply.github.com>
2021-08-16 07:18:03 +0100

    Create CandleSticks.py

---
+++ CandleSticks.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from datetime import datetime
+import plotly.graph_objects as go
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close'])])
+
+fig.show()
